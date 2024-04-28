bceaaa0483f135b63475a23e47a6272f7196f8dd
Haroon Albar <haroon.albar@functionary.co>
2024-04-28 11:28:56 +0530

    type switch

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func do(i interface{}){
+  switch v:= i.(type){
+  case int: fmt.Printf("Bro %v + %v is %v \n",v,v,v+v)
+  case string: fmt.Printf("Bro \"%v\" is %v bytes long\n",v,len(v))
+  default: fmt.Println("I don't know you bro")
+}
+}
+
+func main (){
+  do(10)
+  do("Bugatti")
+  do(false)
+
+}
